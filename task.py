737829766579bca2ac42262d4de360985b1bcbf4
Praveen Tangade <pravintangade122@gmail.com>
2025-12-16 12:52:18 +0530

    Fix formatting and add newline in task.py

--- task.py
+++ task.py
@@ -2,3 +2,6 @@ print("Hello World")
 
 # For using double quotes as String value, you can use the following syntax:
 print('print("Hello World")')
+
+# Using /n to start a new line
+print("Hello\nWorld")
